0c6d2c99146fcb13439f350b9afbfd63fef6b147
trsmith <trsmith@iastate.edu>
2018-12-28 21:23:32 -0500

    completed poor foreign man's bar chart practice project, with french translation

--- chapter1/poor_french_bar_chart.py
+++ chapter1/poor_french_bar_chart.py
@@ -1,15 +1,22 @@
 import pprint
 from collections import defaultdict
+from googletrans import Translator
 
 # simulates a character bar chart with a pretty printed dictionary given user's sentence
+
+
 def main():
 
     while True:
-        sentence_input = input("\nWrite a sentence for a dict bar chart: ""\n")
+        sentence_input = input("\nWrite a sentence for a dict bar chart (en francais): ""\n")
 
-        char_dict = generate_sentence_dict(sentence_input)
+        french_sentence = translate_to_french(sentence_input)
 
+        char_dict = generate_sentence_dict(french_sentence)
+
+        # TODO text part
         # pprint includes newlines based of width and sorts char_dict alphabetically
+        print(french_sentence)
         pprint.pprint(char_dict, width=100)
 
         try_again = input("\n\nTry again? (Press Enter else q to quit)\n ")
@@ -29,6 +36,10 @@ def generate_sentence_dict(sentence_input):
             sentence_dict[c].append(c)
     return sentence_dict
 
+# returns text in french
+def translate_to_french(sentence_input):
+    translator = Translator()
+    return translator.translate(sentence_input, dest='fr').text
 
 if __name__ == "__main__":
     main()
